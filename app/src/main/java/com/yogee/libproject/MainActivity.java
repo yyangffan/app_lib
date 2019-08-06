4d5da86a67262fad334a49e8bdadb7709db8cc3d
yyangffan <1261229101@qq.com>
2019-08-06 13:49:13 +0800

    Initial commit

---
+++ app/src/main/java/com/yogee/libproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.yogee.libproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/********************************************************************
+  @version: 1.0.0
+  @description: 对Library的简单使用
+  @author: EDZ
+  @time: 2019/8/6 13:47
+  @变更历史:
+********************************************************************/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
